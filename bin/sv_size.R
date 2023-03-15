e4f6aca9c9c23b7c2051624998b8a88d8b3e892b
Alexandra Pancikova <alexandra.pancikova@kuleuven.be>
2023-03-15 13:13:08 +0100

    sv visualisation

---
+++ bin/sv_size.R
@@ -0,0 +1,76 @@
+#!/usr/bin/Rscript
+args = commandArgs(trailingOnly=TRUE)
+
+library(ggplot2)
+library(tidyverse)
+library(gridExtra)
+
+#############
+# Deletions #
+#############
+f <- args[1]
+dat <- read.delim(f, header=F, na.strings="", colClasses="character")
+dat$donor <- unlist(strsplit(f,split=".",fixed=T))[1]
+dels <- dat
+
+
+dels$V1 <- as.numeric(dels$V1)
+dels$donor <- as.character(dels$donor)
+dels$donor <- as.factor(dels$donor)
+dels$sampleid<- str_split(dels$donor, '_', simplify = TRUE)[,2]
+
+# as lines
+deletions <- dels %>% 
+  filter(V1>-1000) %>%
+  ggplot(aes(x=V1, color=donor)) +
+  geom_line(aes(y=..count..), stat="bin",binwidth=7, lwd=0.5, show.legend = FALSE)+
+  xlab("Size (bp)") + ylab("Count") + ggtitle("Deletions under 1 000 bp")+
+  theme_minimal() 
+
+# as counts
+#deletions over 1000, but less than 10 000
+deletions_large <- dels %>% 
+  filter(V1< -1000 ) %>%
+  filter(V1>-10000) %>%
+  ggplot(aes(x=V1, color=donor)) + 
+  geom_line(aes(y=..count..), stat="bin",binwidth=500, show.legend = FALSE)+
+  xlab("Size (bp)") + ylab("Count") + ggtitle("Deletions between 1 000-10 000 bp")+
+  theme_minimal()
+
+
+####################
+#insertions ########
+####################
+
+f <- args[2]
+dat <- read.delim(f, header=F, na.strings="", colClasses="character")
+dat$donor <- unlist(strsplit(f,split=".",fixed=T))[1]
+ins <- dat
+
+ins$donor <- as.character(ins$donor)
+ins$V1 <- as.numeric(ins$V1)
+ins$donor <- as.factor(ins$donor)
+ins$sampleid<- str_split(ins$donor, '_', simplify = TRUE)[,2]
+
+#as line
+insertions <- ins %>% 
+  filter(V1<1000) %>%
+  ggplot(aes(x=V1, color=donor)) +
+  geom_line(aes(y=..count..), stat="bin",binwidth=7, alpha=.7, show.legend = FALSE)+
+  xlab("Size (bp)") + ylab("Count") + ggtitle("Insertions under 1 000 bp")+
+  theme_minimal()
+
+insertions_large <- ins %>% 
+  filter(V1>1000 ) %>%
+  filter(V1<10000) %>%
+  ggplot(aes(x=V1, color=donor)) +
+  geom_line(aes(y=..count..), stat="bin",binwidth=500, alpha=.7, show.legend = FALSE)+
+  xlab("Size (bp)") + ylab("Count") + ggtitle("Insertions between 1 000-10 000 bp")+
+  theme_minimal()
+
+########
+# save #
+########
+
+all <- arrangeGrob(deletions,insertions,deletions_large,insertions_large, ncol = 2)
+ggsave(plot = all, args[3], dpi=800, device = "pdf", width = 10, height = 6)
---
+++ modules/r-visualisation.nf
@@ -0,0 +1,55 @@
+// TODO test
+/* 
+*  extract sizes of SVs (del and ins) from SV call
+*/
+
+process extract_SV_lengths {
+    label 'cpu_low'
+    label 'mem_low'
+    label 'time_low'
+    label 'kyber' // just some linux env
+
+    publishDir path: "${params.outdir}/${params.sampleid}/SV_stats/", mode: 'copy'
+
+    input:
+    path variants_pass
+    
+    output:
+    path "${params.sampleid}.deletions.txt", emit: dels // emit path only, since dorado does not accept file, only dir
+    path "${params.sampleid}.insertions.txt", emit: ins
+    path "${params.sampleid}.SV_count.txt"
+
+    script:
+    """
+    less $variants_pass | grep -v "^#" | grep DEL | cut -f8 | cut -d";" -f3 | cut -d"=" -f2 > ${params.sampleid}.deletions.txt
+    less $variants_pass | grep -v "^#" | grep INS | cut -f8 | cut -d";" -f3 | cut -d"=" -f2 > ${params.sampleid}.insertions.txt
+    less $variants_pass | grep -v "^#" | cut -f8 | cut -d';' -f2 | sort | uniq -c > ${params.sampleid}.SV_count.txt
+    """
+}
+
+
+// TODO test
+/* 
+*  plot size distributions
+*/
+
+process plot_SV_lengths {
+    label 'cpu_low'
+    label 'mem_low'
+    label 'time_low'
+    label 'R' 
+
+    publishDir path: "${params.outdir}/${params.sampleid}/SV_stats/", mode: 'copy'
+
+    input:
+    path dels
+    path ins
+    
+    output:
+    path "${params.sampleid}.SV_size_dist.pdf"
+
+    script:
+    """
+    Rscript --vanilla sv_size.R $dels $ins "${params.sampleid}.SV_size_dist.pdf"
+    """
+}
